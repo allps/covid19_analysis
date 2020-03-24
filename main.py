0ed18d6d618cccdcedeb2dd7d97508edcc2a7aef
pragati <pragati@ipropal.com>
2020-03-24 21:17:19 +0530

    exception handling done

--- main.py
+++ main.py
@@ -16,281 +16,291 @@ import os
 import json
 
 async def totalCases(request):
-    dataset_directory_path = os.getcwd() + "/data/"
+    try:
+        dataset_directory_path = os.getcwd() + "/data/"
 
-    dataset_file_list = os.listdir(dataset_directory_path)
+        dataset_file_list = os.listdir(dataset_directory_path)
 
-    # read the dataset
-    covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
+        # read the dataset
+        covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
 
-    covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
-    covid_df.drop(["SNo"], 1, inplace=True)
-    print(covid_df.head())
+        covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
+        covid_df.drop(["SNo"], 1, inplace=True)
+        print(covid_df.head())
 
-    datewise_df = covid_df.groupby(["ObservationDate"]).agg(
-        {"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
-    print(datewise_df.head())
+        datewise_df = covid_df.groupby(["ObservationDate"]).agg(
+            {"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
+        print(datewise_df.head())
 
-    # extract the information from the dataset
-    total_countries_affected = len(covid_df["Country/Region"].unique())
-    total_confirmed_cases = datewise_df["Confirmed"].iloc[-1]
-    total_recovered_cases = datewise_df["Recovered"].iloc[-1]
-    total_deaths = datewise_df["Deaths"].iloc[-1]
+        # extract the information from the dataset
+        total_countries_affected = len(covid_df["Country/Region"].unique())
+        total_confirmed_cases = datewise_df["Confirmed"].iloc[-1]
+        total_recovered_cases = datewise_df["Recovered"].iloc[-1]
+        total_deaths = datewise_df["Deaths"].iloc[-1]
 
-    dictionary = {
-        "totalCountries": total_countries_affected,
-        "confirmed": total_confirmed_cases,
-        "recovered": total_recovered_cases,
-        "deaths": total_deaths
-    }
+        dictionary = {
+            "totalCountries": total_countries_affected,
+            "confirmed": total_confirmed_cases,
+            "recovered": total_recovered_cases,
+            "deaths": total_deaths
+        }
 
-    json_str = json.dumps(dictionary)
-    return JSONResponse(json_str)
-
+        json_str = json.dumps(dictionary)
+        return JSONResponse(json_str)
+    except:
+        return JSONResponse("Something Wrong")
 
 async def confirmed(request):
+    try:
+        dataset_directory_path = os.getcwd() + "/data/"
 
-    # os.chdir(os.getcwd() + "/data/")
-    dataset_directory_path = os.getcwd() + "/data/"
+        dataset_file_list = os.listdir(dataset_directory_path)
 
-    dataset_file_list = os.listdir(dataset_directory_path)
+        # read the dataset
+        covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
 
-    # read the dataset
-    covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
-
-    covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
-    covid_df.drop(["SNo"], 1, inplace=True)
-    print(covid_df.head())
+        covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
+        covid_df.drop(["SNo"], 1, inplace=True)
+        print(covid_df.head())
 
 
-   # grouping different types of cases as per the date
+       # grouping different types of cases as per the date
 
-    datewise_df = covid_df.groupby(["ObservationDate"]).agg({"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
-    print(datewise_df.head())
+        datewise_df = covid_df.groupby(["ObservationDate"]).agg({"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
+        print(datewise_df.head())
 
-    # plot curve of no of confirmed cases
+        # plot curve of no of confirmed cases
 
-    arr_yax = datewise_df['Confirmed'].to_numpy()
-    print(arr_yax)
-    y_list = arr_yax.tolist()
+        arr_yax = datewise_df['Confirmed'].to_numpy()
+        print(arr_yax)
+        y_list = arr_yax.tolist()
 
-    arr_xax = datewise_df.ObservationDate.to_numpy()
-    x_list = arr_xax.tolist()
+        arr_xax = datewise_df.ObservationDate.to_numpy()
+        x_list = arr_xax.tolist()
 
-    dictionary = {
-        "json_xax": x_list,
-        "json_yax": y_list
-    }
+        dictionary = {
+            "json_xax": x_list,
+            "json_yax": y_list
+        }
 
-    json_str = json.dumps(dictionary)
-    return JSONResponse(json_str)
-
+        json_str = json.dumps(dictionary)
+        return JSONResponse(json_str)
+    except:
+        return JSONResponse("something wrong")
 
 async def mortalityRate(request):
+    try:
+        dataset_directory_path = os.getcwd() + "/data/"
 
-    # os.chdir(os.getcwd() + "/data/")
-    dataset_directory_path = os.getcwd() + "/data/"
+        dataset_file_list = os.listdir(dataset_directory_path)
 
-    dataset_file_list = os.listdir(dataset_directory_path)
+        # read the dataset
+        covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
 
-    # read the dataset
-    covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
+        # convert into date time format
 
-    # convert into date time format
+        covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
+        covid_df.drop(["SNo"], 1, inplace=True)
 
-    covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
-    covid_df.drop(["SNo"], 1, inplace=True)
+        datewise_df = covid_df.groupby(["ObservationDate"]).agg({"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
+        datewise_df["Mortality"] = (datewise_df["Deaths"] / datewise_df["Confirmed"]) * 100
 
-    datewise_df = covid_df.groupby(["ObservationDate"]).agg({"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
-    datewise_df["Mortality"] = (datewise_df["Deaths"] / datewise_df["Confirmed"]) * 100
+        arr_yax = datewise_df['Mortality'].to_numpy()
+        print(arr_yax)
+        y_list = arr_yax.tolist()
 
-    arr_yax = datewise_df['Mortality'].to_numpy()
-    print(arr_yax)
-    y_list = arr_yax.tolist()
+        arr_xax = datewise_df.ObservationDate.to_numpy()
+        x_list = arr_xax.tolist()
 
-    arr_xax = datewise_df.ObservationDate.to_numpy()
-    x_list = arr_xax.tolist()
+        dictionary = {
+            "json_xax": x_list,
+            "json_yax": y_list
+        }
 
-    dictionary = {
-        "json_xax": x_list,
-        "json_yax": y_list
-    }
-
-    json_str = json.dumps(dictionary)
-    return JSONResponse(json_str)
+        json_str = json.dumps(dictionary)
+        return JSONResponse(json_str)
+    except:
+        return JSONResponse("Something wrong")
 
 async def recoveryRate(request):
+    try:
+        dataset_directory_path = os.getcwd() + "/data/"
 
-    # os.chdir(os.getcwd() + "/data/")
-    dataset_directory_path = os.getcwd() + "/data/"
+        dataset_file_list = os.listdir(dataset_directory_path)
 
-    dataset_file_list = os.listdir(dataset_directory_path)
+        # read the dataset
 
-    # read the dataset
+        covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
 
-    covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
+        # convert into date time format
 
-    # convert into date time format
+        covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
+        covid_df.drop(["SNo"], 1, inplace=True)
 
-    covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
-    covid_df.drop(["SNo"], 1, inplace=True)
+        datewise_df = covid_df.groupby(["ObservationDate"]).agg({"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
+        datewise_df["Recovery"] = (datewise_df["Recovered"] / datewise_df["Confirmed"]) * 100
 
-    datewise_df = covid_df.groupby(["ObservationDate"]).agg({"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
-    datewise_df["Recovery"] = (datewise_df["Recovered"] / datewise_df["Confirmed"]) * 100
+        arr_yax = datewise_df['Recovery'].to_numpy()
+        print(arr_yax)
+        y_list = arr_yax.tolist()
 
-    arr_yax = datewise_df['Recovery'].to_numpy()
-    print(arr_yax)
-    y_list = arr_yax.tolist()
+        arr_xax = datewise_df.ObservationDate.to_numpy()
+        x_list = arr_xax.tolist()
+        dictionary = {
+            "json_xax": x_list,
+            "json_yax": y_list
+        }
 
-    arr_xax = datewise_df.ObservationDate.to_numpy()
-    x_list = arr_xax.tolist()
-    dictionary = {
-        "json_xax": x_list,
-        "json_yax": y_list
-    }
-
-    json_str = json.dumps(dictionary)
-    return JSONResponse(json_str)
-
+        json_str = json.dumps(dictionary)
+        return JSONResponse(json_str)
+    except:
+        return JSONResponse("Something wrong")
 
 async def countrywise(request):
+    try:
+        req = request.path_params['country']
+        print(req)
+        # os.chdir(os.getcwd() + "/data/")
+        dataset_directory_path = os.getcwd() + "/data/"
 
-    req = request.path_params['country']
-    print(req)
-    # os.chdir(os.getcwd() + "/data/")
-    dataset_directory_path = os.getcwd() + "/data/"
+        dataset_file_list = os.listdir(dataset_directory_path)
+        print(dataset_directory_path + dataset_file_list[0])
 
-    dataset_file_list = os.listdir(dataset_directory_path)
-    print(dataset_directory_path + dataset_file_list[0])
+        # read the dataset
+        covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
+        print(covid_df.head())
+        # convert into date time format
+        covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
+        covid_df.drop(["SNo"], 1, inplace=True)
+        country_data = covid_df[covid_df["Country/Region"] == req]
+        datewise_country = country_data.groupby(["ObservationDate"]).agg({"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
+        print(datewise_country.tail())
 
-    # read the dataset
-    covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
-    print(covid_df.head())
-    # convert into date time format
-    covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
-    covid_df.drop(["SNo"], 1, inplace=True)
-    country_data = covid_df[covid_df["Country/Region"] == req]
-    datewise_country = country_data.groupby(["ObservationDate"]).agg({"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
-    print(datewise_country.tail())
+        arr_yax = datewise_country['Confirmed'].to_numpy()
+        print(arr_yax)
+        y_list = arr_yax.tolist()
 
-    arr_yax = datewise_country['Confirmed'].to_numpy()
-    print(arr_yax)
-    y_list = arr_yax.tolist()
+        arr_xax = datewise_country.ObservationDate.to_numpy()
+        x_list = arr_xax.tolist()
+        dictionary = {
+            "json_xax": x_list,
+            "json_yax": y_list
+        }
 
-    arr_xax = datewise_country.ObservationDate.to_numpy()
-    x_list = arr_xax.tolist()
-    dictionary = {
-        "json_xax": x_list,
-        "json_yax": y_list
-    }
+        json_str = json.dumps(dictionary)
+        return JSONResponse(json_str)
+    except:
+        return JSONResponse("Something Wrong")
 
-    json_str = json.dumps(dictionary)
-    return JSONResponse(json_str)
 
 async def countryMortalityRate(request):
+    try:
+        req = request.path_params['country']
+        print(req)
+        # os.chdir(os.getcwd() + "/data/")
+        dataset_directory_path = os.getcwd() + "/data/"
 
-    req = request.path_params['country']
-    print(req)
-    # os.chdir(os.getcwd() + "/data/")
-    dataset_directory_path = os.getcwd() + "/data/"
+        dataset_file_list = os.listdir(dataset_directory_path)
+        print(dataset_directory_path + dataset_file_list[0])
 
-    dataset_file_list = os.listdir(dataset_directory_path)
-    print(dataset_directory_path + dataset_file_list[0])
+        # read the dataset
+        covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
+        # convert into date time format
+        covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
+        covid_df.drop(["SNo"], 1, inplace=True)
+        country_data = covid_df[covid_df["Country/Region"] == req]
+        datewise_country = country_data.groupby(["ObservationDate"]).agg(
+            {"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
 
-    # read the dataset
-    covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
-    # convert into date time format
-    covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
-    covid_df.drop(["SNo"], 1, inplace=True)
-    country_data = covid_df[covid_df["Country/Region"] == req]
-    datewise_country = country_data.groupby(["ObservationDate"]).agg(
-        {"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
+        datewise_country["Mortality"] = (datewise_country["Deaths"] / datewise_country["Confirmed"]) * 100
 
-    datewise_country["Mortality"] = (datewise_country["Deaths"] / datewise_country["Confirmed"]) * 100
+        arr_yax = datewise_country['Mortality'].to_numpy()
+        print(arr_yax)
+        y_list = arr_yax.tolist()
 
-    arr_yax = datewise_country['Mortality'].to_numpy()
-    print(arr_yax)
-    y_list = arr_yax.tolist()
+        arr_xax = datewise_country.ObservationDate.to_numpy()
+        x_list = arr_xax.tolist()
 
-    arr_xax = datewise_country.ObservationDate.to_numpy()
-    x_list = arr_xax.tolist()
+        dictionary = {
+            "json_xax": x_list,
+            "json_yax": y_list
+        }
 
-    dictionary = {
-        "json_xax": x_list,
-        "json_yax": y_list
-    }
-
-    json_str = json.dumps(dictionary)
-    return JSONResponse(json_str)
-
+        json_str = json.dumps(dictionary)
+        return JSONResponse(json_str)
+    except:
+        return JSONResponse("Something Wrong")
 
 async def countryRecoveryRate(request):
-    req = request.path_params['country']
-    print(req)
-    # os.chdir(os.getcwd() + "/data/")
-    dataset_directory_path = os.getcwd() + "/data/"
+    try:
+        req = request.path_params['country']
+        print(req)
+        # os.chdir(os.getcwd() + "/data/")
+        dataset_directory_path = os.getcwd() + "/data/"
 
-    dataset_file_list = os.listdir(dataset_directory_path)
-    print(dataset_directory_path + dataset_file_list[0])
+        dataset_file_list = os.listdir(dataset_directory_path)
+        print(dataset_directory_path + dataset_file_list[0])
 
-    # read the dataset
-    covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
-    # convert into date time format
-    covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
-    covid_df.drop(["SNo"], 1, inplace=True)
-    country_data = covid_df[covid_df["Country/Region"] == req]
-    datewise_country = country_data.groupby(["ObservationDate"]).agg({"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
+        # read the dataset
+        covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
+        # convert into date time format
+        covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
+        covid_df.drop(["SNo"], 1, inplace=True)
+        country_data = covid_df[covid_df["Country/Region"] == req]
+        datewise_country = country_data.groupby(["ObservationDate"]).agg({"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
 
-    datewise_country["Recovery"] = (datewise_country["Recovered"] / datewise_country["Confirmed"]) * 100
-    arr_yax = datewise_country['Recovery'].to_numpy()
-    print(arr_yax)
-    y_list = arr_yax.tolist()
+        datewise_country["Recovery"] = (datewise_country["Recovered"] / datewise_country["Confirmed"]) * 100
+        arr_yax = datewise_country['Recovery'].to_numpy()
+        print(arr_yax)
+        y_list = arr_yax.tolist()
 
-    arr_xax = datewise_country.ObservationDate.to_numpy()
-    x_list = arr_xax.tolist()
+        arr_xax = datewise_country.ObservationDate.to_numpy()
+        x_list = arr_xax.tolist()
 
-    dictionary = {
-        "json_xax": x_list,
-        "json_yax": y_list
-    }
+        dictionary = {
+            "json_xax": x_list,
+            "json_yax": y_list
+        }
 
-    json_str = json.dumps(dictionary)
-    return JSONResponse(json_str)
+        json_str = json.dumps(dictionary)
+        return JSONResponse(json_str)
+    except:
+        return JSONResponse("Something Wrong")
 
 async def countryTotalCases(request):
-    req = request.path_params['country']
-    print(req)
-    # os.chdir(os.getcwd() + "/data/")
-    dataset_directory_path = os.getcwd() + "/data/"
+    try:
+        req = request.path_params['country']
+        print(req)
+        # os.chdir(os.getcwd() + "/data/")
+        dataset_directory_path = os.getcwd() + "/data/"
 
-    dataset_file_list = os.listdir(dataset_directory_path)
-    print(dataset_directory_path + dataset_file_list[0])
+        dataset_file_list = os.listdir(dataset_directory_path)
+        print(dataset_directory_path + dataset_file_list[0])
 
-    # read the dataset
-    covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
-    # convert into date time format
-    covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
-    covid_df.drop(["SNo"], 1, inplace=True)
-    country_data = covid_df[covid_df["Country/Region"] == req]
-    datewise_country = country_data.groupby(["ObservationDate"]).agg(
-        {"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
+        # read the dataset
+        covid_df = pd.read_csv(dataset_directory_path + dataset_file_list[0])
+        # convert into date time format
+        covid_df["ObservationDate"] = pd.to_datetime(covid_df["ObservationDate"])
+        covid_df.drop(["SNo"], 1, inplace=True)
+        country_data = covid_df[covid_df["Country/Region"] == req]
+        datewise_country = country_data.groupby(["ObservationDate"]).agg(
+            {"Confirmed": 'sum', "Recovered": 'sum', "Deaths": 'sum'}).reset_index()
 
-    # total of different cases per country
-    total_confirmed_cases = datewise_country["Confirmed"].iloc[-1]
-    total_recovered_cases = datewise_country["Recovered"].iloc[-1]
-    total_death_cases = datewise_country["Deaths"].iloc[-1]
+        # total of different cases per country
+        total_confirmed_cases = datewise_country["Confirmed"].iloc[-1]
+        total_recovered_cases = datewise_country["Recovered"].iloc[-1]
+        total_death_cases = datewise_country["Deaths"].iloc[-1]
 
 
-    dictionary = {
-        "confirmed": total_confirmed_cases,
-        "recovered": total_recovered_cases,
-        "deaths": total_death_cases
-    }
-
-    json_str = json.dumps(dictionary)
-
-    return JSONResponse(json_str)
-
+        dictionary = {
+            "confirmed": total_confirmed_cases,
+            "recovered": total_recovered_cases,
+            "deaths": total_death_cases
+        }
+        json_str = json.dumps(dictionary)
+        return JSONResponse(json_str)
+    except:
+        return JSONResponse("Something Wrong")
 
 routes = [
     # Mount('/static', app=StaticFiles(directory='static'), name='static'),
