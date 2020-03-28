b4a202ad37cd99493a40f45bb688546aac359712
Schartz Rehan <rehan.qt@gmail.com>
2020-03-28 14:05:45 +0530

    Minor changes

--- main.py
+++ main.py
@@ -392,4 +392,4 @@ middleware = [
 app = Starlette(debug=True, routes=routes, middleware=middleware)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
