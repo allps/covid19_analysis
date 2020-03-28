014a2477fda622e1b017cdf6caeafaca8300244a
Schartz Rehan <rehan.qt@gmail.com>
2020-03-28 19:30:18 +0530

    Fix issue of load_data.py, line 105

--- dataloader/load_data.py
+++ dataloader/load_data.py
@@ -11,7 +11,7 @@ print("pymongo version:", pymongo.version)
 
 remote_urls = {
     'john_hopkins_repo': 'https://github.com/CSSEGISandData/COVID-19/archive/master.zip',
-    'daily_reports_relative_path': 'COVID-19-master' + os.sep + 'csse_covid_19_data' + os.sep + 'csse_covid_19_daily_reports '
+    'daily_reports_relative_path': 'COVID-19-master' + os.sep + 'csse_covid_19_data' + os.sep + 'csse_covid_19_daily_reports'
 }
 dataset_directory_path = os.getcwd() + os.sep + "data" + os.sep
 
